039bf3f9edcb53e08d060fc7aed465dcce8fdfbb
Ildar1982 <68797569+SalihIldr@users.noreply.github.com>
2022-10-01 07:35:58 +0500

    created a ScriptableObject of the character class

---
+++ Assets/_Scripts/ClassPlayer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Class", menuName = "Class/Create new class", order =51)]
+public class ClassPlayer : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private int _healt;
+    [SerializeField] private int _power;
+    [SerializeField] private int _dexterity;
+}
